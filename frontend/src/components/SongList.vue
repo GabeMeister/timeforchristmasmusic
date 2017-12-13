711394bafecd4a6cc8c90d9e03c1626116a1ed51
Gabe Jensen <thegabejensen@gmail.com>
2017-12-13 06:26:33 -0800

    Forgot to commit new song list component

---
+++ frontend/src/components/SongList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="song-list">
+    <h1 v-text="msg"></h1>
+    <router-link :to="'/song/' + song.url" v-for="song in songs" :key="song.url">
+        <h1 v-text="song.name"></h1>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+
+export default {
+  name: 'Song',
+  data () {
+    return {
+      msg: 'This is the song list page',
+      songs: []
+    }
+  },
+  created: function() {
+      this.getSongList();
+  },
+  methods: {
+      getSongList: function() {
+          axios.get(Vue.config.baseUrl+'/songlist')
+            .then(response => {
+                this.songs = response.data;
+            })
+            .catch(error => {
+                console.log('error: ', error);
+            });
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
